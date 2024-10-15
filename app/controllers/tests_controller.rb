d890bbb13e819f4b043f2f47f0d4539de1563e4b
Pavel <zelenkovpavlik@gmail.com>
2024-10-15 18:59:14 +0300

    Adding advanced features to Simpler

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -3,9 +3,18 @@ class TestsController < Simpler::Controller
   def index
     # render 'tests/list'
     @tests = Test.all
+    # render plain: 'Plain text response'
+    # status(203)
   end
 
   def create
+  end
+
+  def show
+    # render plain: "Test: #{params[:id]}"
+    @params = params[:id]
+    status(201)
+    set_header('Test-Header', 'text/html')
   end
 
 end
